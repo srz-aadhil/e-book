e6055586a66b608e76cfff7033061d7789ff3d1e
srz-aadhil <aadhil.ahammad@smartrabbitz.com>
2024-12-04 17:33:30 +0530

    User update function

--- main.go
+++ main.go
@@ -22,36 +22,45 @@ var err error
 
 // User CRUD
 // User creation function
-func CreateUser(mail, username, password, salt string) (userId int, err error) {
-	query := `INSERT INTO users  (mail, username, password, salt) VALUES($1,$2,$3,$4) RETURNING id`
+// func CreateUser(mail, username, password, salt string) (userId int, err error) {
+// 	query := `INSERT INTO users  (mail, username, password, salt) VALUES($1,$2,$3,$4) RETURNING id`
 
-	if err = db.QueryRow(query, mail, username, password, salt).Scan(&userId); err != nil {
-		fmt.Printf("User creation failed due to %v", err)
-	}
-	return userId, err
-}
+// 	if err = db.QueryRow(query, mail, username, password, salt).Scan(&userId); err != nil {
+// 		fmt.Printf("User creation failed due to %v", err)
+// 	}
+// 	return userId, err
+// }
 
 // Getting one user details function
-func GetUser(id int) (mail, username string, createdat, updatedat time.Time, err error) {
-	query := `SELECT mail,username,created_at,updated_at FROM users WHERE id=$1`
+// func GetUser(id int) (mail, username string, createdat, updatedat time.Time, err error) {
+// 	query := `SELECT mail,username,created_at,updated_at FROM users WHERE id=$1`
 
-	if err = db.QueryRow(query, id).Scan(&mail, &username, &createdat, &updatedat); err != nil {
-		log.Printf("User details fetching failed due to %s", err)
-	}
+// 	if err = db.QueryRow(query, id).Scan(&mail, &username, &createdat, &updatedat); err != nil {
+// 		log.Printf("User details fetching failed due to %s", err)
+// 	}
 
-	fmt.Println("User fetching successfull")
-	return mail, username, createdat, updatedat, nil
-}
+// 	fmt.Println("User fetching successfull")
+// 	return mail, username, createdat, updatedat, nil
+// }
 
 // Getting all users fucntion
 // func GetAllUsers() ()
 
 // Update user function
-func updateUser(id int, mail, password string) (err error) {
-	query := `UPDATE users SET mail=$1,password=$2,updated_at=&3,id =$4`
+func UpdateUser(id int, mail, password string) (err error) {
+	query := `UPDATE users SET mail=$1,password=$2,updated_at=$3 WHERE id =$4`
 
-	if _, err := db.Exec(query, id, mail, password); err != nil {
+	result, err := db.Exec(query, mail, password, time.Now().UTC(), id)
+	if err != nil {
 		return fmt.Errorf("User Updation failed due to %v", err)
+	}
+
+	isAffected, err := result.RowsAffected()
+	if err != nil {
+		return fmt.Errorf("no affected rows due to : %w", err)
+	}
+	if isAffected == 0 {
+		return fmt.Errorf("no user with ID : %d", id)
 	}
 
 	log.Println("User updation successfully completed")
@@ -89,10 +98,7 @@ func main() {
 	// fmt.Printf("User details are \n mail-%s, username-%s, createdat-%v, updatedat-%v", mail, username, createdat, updatedat)
 
 	//Updating a user
-	updateUser(1, "userupdated@srz.com", "newpassword")
-	if err != nil {
-		log.Print(err)
-		return
+	if err := UpdateUser(1, "userupdated@srz.com", "newpassword"); err != nil {
+		log.Println(err)
 	}
-
 }
